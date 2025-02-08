57c4b30fa0f36429774eb6176c2d31e1e2102c60
Rishabh Gupta <guptarishabh1122@gmail.com>
2025-02-08 15:02:09 +0530

    captain authentication completed

--- Backend/controllers/captain.controller.js
+++ Backend/controllers/captain.controller.js
@@ -1,6 +1,7 @@
 const captainModel = require("../models/captain.model");
 const captainService = require("../services/captain.service");
 const { validationResult } = require("express-validator");
+const blacklistTokenModel = require("../models/blacklistToken.model");
 
 module.exports.registerCaptain = async (req, res, next) => {
   const errors = validationResult(req);
@@ -33,3 +34,48 @@ module.exports.registerCaptain = async (req, res, next) => {
    return res.status(201).json({token, captain});
 
 };
+
+
+module.exports.loginCaptain = async( req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+  
+    const { email, password} = req.body;
+  
+    const captain = await captainModel.findOne({email}).select("+password");
+
+    if(!captain){
+        return res.status(401).json({message: "Invalid email or password"})
+    }
+
+    const isMatch = await captain.comparePassword(password);
+
+    if(!isMatch){
+        return res.status(401).json({message: "Invalid email or password"})
+    }
+
+    const token = captain.generateAuthToken();
+    
+    //setting cookie with this token
+    const cookie = res.cookie('token', token);
+
+
+    return res.status(200).json({token, captain});
+
+
+}
+
+module.exports.getCaptainProfile = async( req, res, next) => {
+    return res.status(200).json(req.captain)
+}
+
+module.exports.logoutCaptain = async(req, res, next) => {
+    const token = req.cookies.token || req.headers.authorization.split(' ')[1];
+    await blacklistTokenModel.create({token});
+    res.clearCookie('token');
+
+
+    return res.status(200).json({message: 'Logged out'});
+}
